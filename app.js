a71e5ece4550c1fbf4c57b561f356767c07b7564
Subasri1803 <70086579+Subasri1803@users.noreply.github.com>
2020-10-11 03:33:26 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope,$timeout) {
+    $scope.names = ["general", "moral", "mystery","comic"];
+	 $scope.myHeader = "Helloooo";
+  $timeout(function () {
+      $scope.myHeader = "Ready to create your storyyyy????";
+  }, 2000);
+   $scope.disableClick = function (isDisabled) { 
+            $scope.isDisabled = !isDisabled; 
+            } 
+			
+	$scope.values=[{name:'The history of Tom Jones',Author:'Henry Fielding'},
+		    {name:'Pride and Prejudice',Author:'Jane Austen'},
+			{name:'The red and black',Author:'Stendhal'}];
+			
+	$scope.kp2=1000;
+	 $scope.friends = [
+    {name: 'History of Tom jones', author:'Henry Fielding'},
+    {name: 'Pride and Prejudice', author:'Jane Austen'},
+    {name: 'The red and black', author:'Stendhal'}
+  ];
+    $scope.show=false;
+	
+	 $scope.users = { 
+                user: "" 
+            }; 
+            $scope.login = function () { 
+                $scope.isShow = true; 
+                $scope.name = $scope.users.user; 
+                $scope.users = { 
+                    user: "" 
+                } 
+            }; 
+			
+	$scope.timeofday='we hope';
+	$scope.greet='it is interesting';
+  
+});
+
+app.controller('yrctrl',function($scope){
+	   $scope.greet='you are learning';
+});
+
